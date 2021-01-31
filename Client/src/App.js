316b1660e8f6d65e22bbbc69fa7cf4f90ebc3f9e
Spuros <spurakos98@gmail.com>
2021-01-31 13:03:55 +0200

    Initialize Register/Login

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,65 @@
+import React, {useState} from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+    const [usernameReg, setUsernameReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
+
+    const [usernameLog, setUsernameLog] = useState('')
+    const [passwordLog, setPasswordLog] = useState('')
+
+    const [loginStatus, setLoginStatus] = useState('')
+
+    const register = () =>{
+        Axios.post("http://localhost:3001/register", {username: usernameReg, password: passwordReg}).then((response)=>{
+            console.log(response.data)
+        });
+    }
+
+    const login = () =>{
+        Axios.post("http://localhost:3001/login", {username: usernameLog, password: passwordLog}).then((response)=>{
+            if(response.data.message){
+                setLoginStatus(response.data.message)
+            }
+            else{
+                setLoginStatus(response.data[0].username)
+            }
+            
+        });
+    }
     return (
     <div className="App" >
         <div className="registration">
             <h1>Registration</h1>
+
             <label>Username</label>
-            <input type="text"></input>
+            <input type="text"
+                onChange={(event)=>{setUsernameReg(event.target.value)}}>
+            </input>
+
             <label>Password</label>
-            <input type="password"></input>
-            <button>Register</button>
+            <input type="password"
+                onChange={(event)=>{setPasswordReg(event.target.value)}}>
+            </input>
+
+            <button on onClick={register}>Register</button>
         </div>
         <div className="login">
         <h1>Login</h1>
             <label>Username</label>
-            <input type="text"></input>
+            <input type="text" 
+                onChange={(event)=>{setUsernameLog(event.target.value)}}>
+            </input>
+
             <label>Password</label>
-            <input type="password"></input>
-            <button>Login</button>
+            <input type="password" 
+                onChange={(event)=>{setPasswordLog(event.target.value)}}>
+            </input>
+            <button onClick={login}>Login</button>
         </div>
+        <h1>{loginStatus}</h1>
 
     </div>
     );
--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const e = require('express');
+
+const saltRounds = 10;
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +18,80 @@ const db = mysql.createConnection({
     database: 'jwtdb'
 })
 
+
+//Register
+app.post('/register', (req, res)=>{
+
+    const username = req.body.username
+    const password = req.body.password
+
+    console.log(req.body)
+
+    bcrypt.hash(password, saltRounds, (err,hash) =>{
+        
+        db.query('INSERT INTO users (username, password) VALUES(?,?)',
+        [username, hash], (error, result)=>{
+             if(error){
+                 console.log(error)
+             }else{
+                 res.send("Values Inserted")
+             }
+         });
+    
+
+    })
+
+  
+});
+
+//Login
+app.post('/login', (req, res)=>{
+
+    const username = req.body.username
+    const password = req.body.password
+
+    console.log(req.body)
+
+    db.query('SELECT * FROM users WHERE username = ?',
+    [username],
+    (error, result)=>{
+         if(error){
+             res.send({error: error})
+         }
+        
+        if(result.length>0){
+
+            bcrypt.compare(password,result[0].password, (error, response)=>{
+                if(response){
+                    res.send(result)    
+                }else{
+                    res.send({message: "Wrong username and password combination"})
+                }
+                
+
+            })
+        
+        }else{
+            res.send({message: "This user doesn't exist"})
+        }
+                
+         
+     });
+
+});
+
+//Get 
+app.get('/employees', (req, res) => {
+    
+    
+    db.query("SELECT * FROM employees", (error, result) => {
+        if(error){
+            console.log(error)
+        }else{
+            res.send(result)
+        }
+    });
+});
 
 
 app.listen(3001, ()=>{console.log("Running on 3001")});
